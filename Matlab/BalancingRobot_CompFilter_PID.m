0ee4e408bddc038b916aae4612aa21809fd22dbf
apubambu <32548332+apubambu@users.noreply.github.com>
2020-01-16 15:25:54 +0100

    -funktionierende gains

--- Matlab/BalancingRobot_CompFilter_PID.m
+++ Matlab/BalancingRobot_CompFilter_PID.m
@@ -6,22 +6,11 @@ clear all
 group = HebiLookup.newGroupFromNames('Robot',{'1'; '2'; '3'; '4'}); 
 
 % Define new group with mobile IO app
-
 cmd = CommandStruct();
 
+%% PARAMS and KINEMATICS
 % Mecanum Wheeled Robot Parameters
 [Rw,l1,l2,alpha] = MecanumPendulum();
-
-% PID Controller Gains
-
-% Maximum velocities
-
-% Declare error variables
-esum = 0; % starting value for cumulative error
-e = 0; % current error
-eold = 0; % previous error
-
-target = 0; % theta_p = 0
 
 % inverse kinematic mapping
 M = 1/Rw * [cot(alpha(1)) 1 l2 -Rw;...
@@ -29,41 +18,79 @@ M = 1/Rw * [cot(alpha(1)) 1 l2 -Rw;...
     cot(alpha(3)) 1 -l1 -Rw;...
     cot(alpha(4)) 1 -l2 Rw];
 
+%% COMPLEMENTARY FILTER
 % Time constants for complementary filter
-tau = 0.5; %[s]
-Ts = 1/group.getFeedbackFrequency; %[s], default feedback frequency is 100 Hz 
+tau     = 0.5; %[s]
+Ts      = 1/group.getFeedbackFrequency; %[s], default feedback frequency is 100 Hz 
 alpha_c = tau/(tau+Ts);
-% initial phi
 
-t_old = group.getNextFeedback.time; % module time at start
+%% INNER LOOP
+% Declare error variables
+esum    = 0; % starting value for cumulative error
+e       = 0; % current error
+eold    = 0; % previous error
+% Gains
+Kp      = 0.8; %1.1
+Ki      = 15; %0.9
+Kd      = 0;
+%SetPoint
+SetPoint = 0;
 
-count = 1;
+%% OUTER LOOP 
+Kp_theta_phi    = 0.005;
+Ki_theta_phi    = 0.008
+SetPoint_Theta  = 0;
+e_ol            = 0;
+esum_ol         = 0;
+%
+
+
+%% INITIAL VALUES
+t_old   = group.getNextFeedback.time; % module time at start
+count   = 1;
+phi     = 0;
+phi_dot = 0;
+dt_log  = [];
+
 
 while 1
-    fbk = group.getNextFeedback(); % get new feedback
-    t = fbk.time; % get time form feedback struct
-    dt = t-t_old; % compute time from last to current feedback
-    t_old = t; % save current time
     
-    
-    
-    % calculate theta_p with a complementary filter
+    fbk     = group.getNextFeedback();  % get new feedback
+    t       = fbk.time;                 % get time form feedback struct
+    dt      = t-t_old;                  % compute time from last to current feedback
+    t_old   = t;                        % save current time
 
-    phi_dot = alpha_c*(phi_dot + fbk.gyroZ(1)) + (1-alpha_c)*atan2(fbk.accelY(1),fbk.accelX(1))/dt; % Complementary filter (evtl. -fbk.gyroZ)
-
+    %% MEASUREMENT VARIABLES
+    % phi
+    phi         = alpha_c*(phi + fbk.gyroZ(1) * dt) + (1 - alpha_c) * atan(-fbk.accelY(1)/ fbk.accelX(1)) % Complementary filter (evtl. -fbk.gyroZ)
+    phi_dot     = fbk.gyroZ(1);
+    theta_dot   = -mean([-fbk.velocity(1), fbk.velocity(2), -fbk.velocity(3), fbk.velocity(4)]) ;
     
-    esum = esum + e; %esum = esum + e*dt;
-    y = Kp * e + Ki * dt * esum + Kd * (e - eold)/dt; %y = Kp * e + Ki * esum + Kd * (e - eold)/dt;
-    eold = e;
+    %% OUTER LOOP
+    e_ol = SetPoint_Theta - theta_dot;
+    esum_ol = esum_ol + e_ol;
+    SetPoint_ = Kp_theta_phi * (e_ol) + ...
+                Ki_theta_phi * dt *(esum_ol);
+    
+    %% INNER LOOP CONTROL
+    e       = SetPoint_  - phi;
+    esum    = esum + e; 
+    y       = Kp * e +...
+              Ki * dt * esum +...
+              Kd * (e - eold)/dt;
+    eold    = e;
     
     
+    %% OUTPUT
+    q_dot     = [ 0; 0; 0; y/dt];
     % calculate wheel velocities
     theta_dot = M * q_dot;
-    
-    count = count + 1;
-    
+
     % Hebi updaten
     cmd.velocity = theta_dot';
     group.send(cmd);
+    
+    %% LOGS
+    dt_log = [dt_log dt];
 end
 
